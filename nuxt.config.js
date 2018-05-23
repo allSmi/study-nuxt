8e1fb91a71fb3433f8ddb6d8f66115653425da40
__Steel笑口 <kewangxunqiu@163.com>
2018-05-23 22:53:51 +0800

    add favicon.ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ module.exports = {
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
             { hid: 'description', name: 'description', content: 'Meta description' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
     css: [
